1c722ae1df398cfbcc52005a2a54b02416d3abc0
Yuhui Yao <bolus123@users.noreply.github.com>
2020-08-01 16:14:51 -0500

    update

--- R/headerEWMA.R
+++ R/headerEWMA.R
@@ -16,7 +16,7 @@ getARL <- function(L, lambda, m, nu, n, ubc, tmin, tmax, state = 'steady') {
 }
 
 
-getCCEWMAUncond <- function(ARL0, interval, lambda, m, nu = m - 1, n = 5, ubc = 1, t.interval = c(20, 50), state = 'steady', tol = 1e-2) {
+getCC.CUC <- function(ARL0, interval = c(1, 4), lambda, m, nu = m - 1, n = 5, ubc = 1, t.interval = c(20, 50), state = 'steady', tol = 1e-2) {
   
   rootFinding <- function(L, ARL0, lambda, m, nu, n, ubc, tmin, tmax, state = 'steady') {
     
@@ -57,7 +57,7 @@ pCARLEWMA <- function(q, L, lambda, m, nu, n, ubc, tmin, tmax, state = 'steady')
 
 
 
-getCCEWMACond <- function(p0, interval = c(1, 4), ARL0, eps, lambda, m, nu = m - 1, n = 5, ubc = 1, t.interval = c(20, 50), state = 'steady', tol = 1e-4) {
+getCC.EPC <- function(p0, interval = c(1, 4), ARL0, epstilda, lambda, m, nu = m - 1, n = 5, ubc = 1, t.interval = c(20, 50), state = 'steady', tol = 1e-2) {
   
   rootFinding <- function(L, p0, ARLb, lambda, m, nu, n, ubc, tmin, tmax, state) {
     
@@ -70,9 +70,208 @@ getCCEWMACond <- function(p0, interval = c(1, 4), ARL0, eps, lambda, m, nu = m -
   tmin <- t.interval[1]
   tmax <- t.interval[2]
  
+  eps <- epstilda
+ 
   ARLb <- (1 - eps) * ARL0
   
   uniroot(rootFinding, interval = interval, p0 = p0, ARLb = ARLb, lambda = lambda, m = m, nu = nu, n = n, ubc = ubc, 
           tmin = tmin, tmax = tmax, state = state, tol = tol)$root
   
+}
+
+
+getCC <- function(
+          m,
+          nu = m - 1,
+		  n = 5,
+          ARL0 = 370,
+          interval = c(1, 4),
+		  lambda = 0.1,
+          EPC.p0 = 0.05,
+          EPC.epstilda = 0,
+          cc.option = c('EPC'),
+		  ubCons = 1, 
+		  t.interval = c(20, 50),
+		  state = 'steady',
+		  tol = 1e-2) 
+{
+
+  if (cc.option == 'CUC') {
+
+    cc <- getCC.CUC(
+      ARL0 = ARL0, 
+	  interval = interval, 
+	  lambda = lambda, 
+	  m = m, 
+	  nu = nu, 
+	  n = n, 
+	  ubc = ubc, 
+	  t.interval = t.interval, 
+	  state = state, 
+	  tol = tol
+    )
+
+  } else if (cc.option == 'EPC') {
+
+    cc <- getCC.EPC(
+	  p0 = EPC.p0, 
+	  interval = interval, 
+	  ARL0 = ARL0, 
+	  epstilda = EPC.epstilda, 
+	  lambda = lambda, 
+	  m = m, 
+	  nu = nu, 
+	  n = n, 
+	  ubc = ubc, 
+	  t.interval = t.interval, 
+	  state = state, 
+	  tol = tol
+	)
+	
+  }
+
+  return(cc)
+
+}
+
+
+
+PH2EWMA <- function(
+  X2,
+  X1,
+  cc = NULL,
+  ARL0 = 370,
+  interval = c(1, 4),
+  lambda = 0.1,
+  EPC.p0 = 0.05,
+  EPC.epstilda = 0,
+  cc.option = c('EPC'),
+  ubCons = 1, 
+  t.interval = c(20, 50),
+  state = 'steady',
+  tol = 1e-2
+  ubCons.option = TRUE,
+  plot.option = TRUE) 
+{
+
+  m <- dim(X1)[1]
+  nu <- m - 1
+  n <- dim(X1)[2]
+  
+  if (ubCons.option == TRUE) {
+
+    ubCons = c4.f(nu)
+
+  } else {
+
+    ubCons = 1
+
+  }
+
+  m2 <- dim(X2)[1]
+
+  X1bar <- rowMeans(X1)
+  X1barbar <- mean(X1bar)
+  X1Var <- var(X1bar)
+
+  X2bar <- rowMeans(X2)
+
+  if (is.null(cc)) {
+
+    cc <- rep(NA, 2)
+    txt1 <- rep(NA, 2)
+	txt2 <- rep(NA, 2)
+	
+    lower.limits <- rep(NA, 2)
+    upper.limits <- lower.limits
+
+    if (length(which(cc.option == 'CUC')) > 0) {
+
+      cc[1] <- getCC.CUC(
+					ARL0 = ARL0, 
+					interval = interval, 
+					lambda = lambda, 
+					m = m, 
+					nu = nu, 
+					n = n, 
+					ubc = ubc, 
+					t.interval = t.interval, 
+					state = state, 
+					tol = tol
+				)
+			  
+	   txt1[1] <- 'CUC'
+	   txt2[1] <- 'CUC'
+
+    }
+
+    if (length(which(cc.option == 'EPC')) > 0) {
+
+      cc[2] <- getCC.EPC(
+					p0 = EPC.p0, 
+					interval = interval, 
+					ARL0 = ARL0, 
+					epstilda = EPC.epstilda, 
+					lambda = lambda, 
+					m = m, 
+					nu = nu, 
+					n = n, 
+					ubc = ubc, 
+					t.interval = t.interval, 
+					state = state, 
+					tol = tol
+				)
+	    
+		txt1[2] <- 'EPC'
+		txt2[2] <- 'EPC'
+
+    }
+
+  } else {
+  
+		cc.num <- length(cc)
+		txt1 <- rep(NA, cc.num)
+		txt2 <- rep(NA, cc.num)
+		
+		for (i in 1:cc.num) {
+			txt1[i] <- paste('LCL', i, sep = '')
+			txt2[i] <- paste('UCL', i, sep = '')
+		}
+  }
+
+  cc <- cc[!is.na(cc)]
+
+  cc.num <- length(cc)
+
+  lower.limits <- X1barbar - cc * sqrt(X1Var) / ubCons
+  upper.limits <- X1barbar + cc * sqrt(X1Var) / ubCons
+
+  if (plot.option == TRUE) {
+
+    plot(c(1, m2), c(min(X2bar, lower.limits), max(X2bar, upper.limits)), type = 'n',
+          xlab = 'Subgroup', ylab = 'Sample Mean')
+    points(1:m2, X2bar, type = 'o', lty = 1)
+
+    for (i in 1:cc.num) {
+
+      abline(h = lower.limits[i], lty = i)
+      text(round(m2 * 0.8), lower.limits[i], paste(txt1[i], ' = ', round(lower.limits[i], 4)), pos = 3)
+
+      abline(h = upper.limits[i], lty = i)
+      text(round(m2 * 0.8), upper.limits[i], paste(txt2[i], ' = ', round(upper.limits[i], 4)), pos = 1)
+
+    }
+
+  }
+
+  out <- list(
+            CL = X1barbar,
+            sigma = sqrt(X1Var) / ubCons,
+            PH2.cc = cc,
+            LCL = lower.limits,
+            UCL = upper.limits,
+            CS = X2bar)
+
+  return(out)
+
 }
